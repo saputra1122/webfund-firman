efad411050866842b0558b286e1ae5502eab281e
saputra1122 <64419876+saputra1122@users.noreply.github.com>
2021-06-17 21:35:33 +0700

    Delete webcounter.php

--- webcounter.php
+++
@@ -1,9 +0,0 @@
-<?php 
-				$filetext = "webcounter.txt";
-				$counter  = file($filetext);
-				$visitor  = $counter[0];
-				$visitor++;
-				$file     = fopen($filetext,"w");
-				fwrite($file, $visitor);
-				fclose($file);
-			?>
